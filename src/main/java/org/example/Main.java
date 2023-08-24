58d75f9fdb917c6e0bf0b13c08b5b5db6542b0fe
LuzRdgz <luzrdgz15@gmail.com>
2023-08-23 21:50:54 -0700

    Ejercicio I y II

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,35 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scan = new Scanner(System.in);
+    static String lectTeclado;
+    static int posicion;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Introduzca una cadena: ");
+        lectTeclado = scan.nextLine();
+        System.out.println("Posición buscada (Desde la posición 0): ");
+        posicion = scan.nextInt();
+        scan.close();
+
+        Main charEn = new Main();
+
+        try {
+           System.out.println("Carácter de la posición " + posicion + " encontrado: " + charEn.caracterEn(lectTeclado, posicion));
+        } catch (Exception e){
+            System.out.println("Excepcion: " + e.getMessage());
+        }
+
+    }
+
+    char caracterEn(String lectTeclado, int posicion) throws Exception {
+        if (posicion < 0 || lectTeclado.length() < posicion){
+            throw new Exception("Has intentado recuperar una posición de la cadena de caracteres que no existe.");
+        }
+        return lectTeclado.charAt(posicion);
     }
 }
